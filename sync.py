2c1c817902eab321bbea74ed0613c94965347811
Evan Mattiza <emattiza@gmail.com>
2019-01-20 11:03:18 -0600

    added bpython, basic repo printing

--- sync.py
+++ sync.py
@@ -2,6 +2,13 @@ import os
 from errors import EnvironmentArgError
 from github import Github
 
+
+def print_repos(ghub):
+    for repo in ghub.get_user().get_repos():
+        print(repo.name)
+    print(repo)
+
+
 if __name__ == "__main__":
     TOKEN = os.environ.get("GITHUB_API_TOKEN", None)
     if TOKEN:
@@ -11,3 +18,5 @@ if __name__ == "__main__":
             "Could not find GITHUB_API_TOKEN in"
             + " environment. Please set to access API!"
         )
+    if G:
+        print_repos(G)
